d1b262c07fc2019a55b900c3cc108c896f3b6508
tobias727 <tobias.zenner@d-fine.com>
2025-09-24 21:36:11 +0200

    Refactor main.py

--- src/main.py
+++ src/main.py
@@ -9,52 +9,33 @@ from src.config import (
     TOKEN0_INPUT,
 )
 
+TESTNET = True
 
 def main():
     """Entrypoint for trading bot"""
-    ## Logging and saving logs
-    log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-    # Console handler
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-    # File handler
-    log_dir = "out/logs"
-    os.makedirs(log_dir, exist_ok=True)
-    log_file = os.path.join(log_dir, "trading_bot.log")
-    file_handler = logging.FileHandler(log_file, mode="a")
-    file_handler.setFormatter(log_formatter)
-    # Logger setup
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logger.propagate = False  # avoids double logging
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-    orchestrator = Orchestrator(logger)
-    detector = Detector(logger)
-    binance = BinanceClientRpc(logger, TOKEN0_INPUT, testnet=True)
-
-    # Load pools from JSON
-    pools_filepath = "unichain_v4_pools.json"
-    pools = load_pools(pools_filepath)
-    uniswap = UnichainV4Client(
-        pools, logger, testnet=True
-    )  # TODO: , testnet=True, fix get_amounts_out self.web3
+    logger = setup_logger("trading_bot")
+    binance, uniswap, orchestrator, detector = init_clients(logger, testnet=TESTNET)
 
     while True:
         # 1. uniswap quote, 2. binance quote, 3. binance execution, 4. uniswap execution
-        uniswap_bid_notional, uniswap_ask_notional = (
-            uniswap.estimate_swap_price()
-        )  # returns [price, gas]
-        logger.info(
-            "Uniswap: %s, %s",
-            f"{uniswap_bid_notional[0]:_}",
-            f"{uniswap_ask_notional[0]:_}",
-        )
-        binance_bid_notional, binance_ask_notional = binance.get_price()
-        logger.info(
-            "Binance: %s, %s", f"{binance_bid_notional:_}", f"{binance_ask_notional:_}"
-        )
+        try:
+            uniswap_bid_notional, uniswap_ask_notional = (
+                uniswap.estimate_swap_price()
+            )  # returns [price, gas]
+            logger.info(
+                "Uniswap: %s, %s",
+                f"{uniswap_bid_notional[0]:_}",
+                f"{uniswap_ask_notional[0]:_}",
+            )
+            binance_bid_notional, binance_ask_notional = binance.get_price()
+            logger.info(
+                "Binance: %s, %s", f"{binance_bid_notional:_}", f"{binance_ask_notional:_}"
+            )
+        # Graceful retry for quoting connection error
+        except ConnectionError as e:
+            logger.error("Iteration skipped due to data fetch error: %s", e)
+            continue
+
         side, edge = detector.detect(
             binance_bid_notional,
             binance_ask_notional,
@@ -73,6 +54,38 @@ def main():
                 receipt_unichain,
             )
 
+def init_clients(logger, testnet: bool = False):
+    """Initializes clients"""
+    binance = BinanceClientRpc(logger, TOKEN0_INPUT, testnet)
+    pools = load_pools("unichain_v4_pools.json")
+    uniswap = UnichainV4Client(pools, logger, testnet)
+    orchestrator = Orchestrator(logger)
+    detector = Detector(logger)
+    return binance, uniswap, orchestrator, detector
+
+def setup_logger(name: str, log_dir: str = "out/logs") -> logging.Logger:
+    """Create and configure logger with console + file handlers."""
+    log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = os.path.join(log_dir, f"{name}.log")
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+
+    # File handler
+    file_handler = logging.FileHandler(log_file, mode="a")
+    file_handler.setFormatter(log_formatter)
+
+    # Logger setup
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
 
 if __name__ == "__main__":
     main()
