34b01dfed0d2836c95f1260a20ef39c91c545f4f
tobias727 <tobias.zenner@d-fine.com>
2026-01-04 16:05:53 +0100

    Save logs to dir

--- startTrading.sh
+++ startTrading.sh
@@ -3,31 +3,34 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VENV_PATH="$SCRIPT_DIR/.venv/bin/activate"
 PYTHON_SCRIPT="src/main.py"
 REQUIREMENTS_FILE="$SCRIPT_DIR/requirements.txt"
+LOG_DIR="$SCRIPT_DIR/out/logs"
+LOG_FILE="$LOG_DIR/trading_bot_v.4.0.log"
 
 cd "$SCRIPT_DIR" || exit 1
 
+mkdir -p "$LOG_DIR"
+
 if [[ ! -f "$VENV_PATH" ]]; then
-    echo -e "❌ Error: Virtual environment not found. Ensure .venv exists."
+    echo "❌ Error: Virtual environment not found. Ensure .venv exists."
     exit 1
 fi
 
-echo -e "⚙️ Activating virtual environment..."
+echo "⚙️ Activating virtual environment..."
 source "$VENV_PATH"
 
 if [[ -f "$REQUIREMENTS_FILE" ]]; then
-    echo -e "🔍 Checking for missing dependencies..."
+    echo "🔍 Checking for missing dependencies..."
     pip install -r "$REQUIREMENTS_FILE" --quiet
-
     if [[ $? -ne 0 ]]; then
-        echo -e "❌ Error: Failed to install dependencies from requirements.txt."
+        echo "❌ Error: Failed to install dependencies from requirements.txt."
         exit 1
     fi
 else
-    echo -e "❌ Error: requirements.txt not found in the script directory."
+    echo "❌ Error: requirements.txt not found in the script directory."
     exit 1
 fi
 
 export PYTHONPATH="$SCRIPT_DIR:$PYTHONPATH"
 
-echo -e "⚡ Starting Execution..."
-python3 "$PYTHON_SCRIPT"
+echo "⚡ Starting Execution... logs: $LOG_FILE"
+python3 "$PYTHON_SCRIPT" >> "$LOG_FILE" 2>&1
