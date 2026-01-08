2635a8f2d0e24b8be1bf488d7a5bedd47b980f2f
tobias727 <tobias.zenner@d-fine.com>
2026-01-08 20:42:20 +0100

    Bump image version to v1.4.0

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY src ./src
-COPY values_local.yaml .
-COPY values_remote.yaml .
+COPY values.yaml .
 
 CMD ["python", "-m", "src.main"]
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t tobias875/dex-arb-trader:v1.3.0 .
-docker push tobias875/dex-arb-trader:v1.3.0
+docker build -t tobias875/dex-arb-trader:v1.4.0 .
+docker push tobias875/dex-arb-trader:v1.4.0
 docker image prune -f
